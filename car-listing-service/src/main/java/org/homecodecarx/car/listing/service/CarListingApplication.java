c6090868a1007ed9dce8a92c7373b789c97bf331
DraganovTS <cvetomir_draganov@abv.bg>
2026-01-05 20:01:32 +0200

    Initialize multi-module Maven project with services for Admin, User, Notification, Car Listing, and Marketplace, including common configurations and dependencies.

---
+++ car-listing-service/src/main/java/org/homecodecarx/car/listing/service/CarListingApplication.java
@@ -0,0 +1,11 @@
+package org.homecodecarx.car.listing.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarListingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarListingApplication.class, args);
+    }
+}
